915e7a02655bb98c34614be4ed8ce0d74abd74c7
yagiey <yagiey@gmail.com>
2022-11-13 21:59:25 +0900

    add operator Div

--- CalcLib/Operators.cs
+++ CalcLib/Operators.cs
@@ -5,5 +5,6 @@
 		static public int Add(int a, int b) { return a + b; }
 		static public int Sub(int a, int b) { return a - b; }
 		static public int Mul(int a, int b) { return a * b; }
+		static public double Div(int a, int b) { return ((double)a) / b; }
 	}
 }
