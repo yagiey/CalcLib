a98e520877f21baccc60c181807c1d028e4ad9cc
yagiey <yagiey@gmail.com>
2022-11-13 22:03:10 +0900

    use operator =>

--- CalcLib/Operators.cs
+++ CalcLib/Operators.cs
@@ -2,9 +2,9 @@
 {
 	public static class Operators
 	{
-		static public int Add(int a, int b) { return a + b; }
-		static public int Sub(int a, int b) { return a - b; }
-		static public int Mul(int a, int b) { return a * b; }
-		static public double Div(int a, int b) { return ((double)a) / b; }
+		static public int Add(int a, int b) => a + b;
+		static public int Sub(int a, int b) => a - b;
+		static public int Mul(int a, int b) => a * b;
+		static public double Div(int a, int b) => ((double)a) / b;
 	}
 }
